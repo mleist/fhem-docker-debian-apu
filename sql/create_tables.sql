b33d8301d4988d0b8dc456bf5a164a4d8743b7ad
Markus Leist <markus@lei.st>
2021-08-26 09:59:13 +0200

    write down more know-how

---
+++ sql/create_tables.sql
@@ -0,0 +1,4 @@
+CREATE USER fhem_cfg WITH PASSWORD 'passw0rd_cfg';
+CREATE USER fhem_log WITH PASSWORD 'passw0rd_log';
+CREATE DATABASE fhem_cfg WITH OWNER fhem_cfg;
+CREATE DATABASE fhem_log WITH OWNER fhem_log;
