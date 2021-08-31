5d80cf75d541e727d960d3b20144d2a79a22fdf8
Markus Leist <markus@lei.st>
2021-08-31 22:00:51 +0200

    docker-bin from docker

--- stage3_postinstall_apu/first_boot.sh
+++ stage3_postinstall_apu/first_boot.sh
@@ -4,12 +4,14 @@ FLAG="/var/log/firstboot.log"
 if [ ! -f $FLAG ]; then
    #Put here your initialization sentences
    echo "This is the first boot"
+   set -x
 
    cd /opt
    ssh-keyscan -H github.com >> /root/.ssh/known_hosts
    git clone git@github.com:mleist/fhem-docker-debian-apu.git
 
-   /opt/fhem-docker-debian-apu/stage3_postinstall_apu/11_install_ctop.sh
+   /opt/fhem-docker-debian-apu/stage3_postinstall_apu/11_install_docker.sh
+   /opt/fhem-docker-debian-apu/stage3_postinstall_apu/12_install_ctop.sh
 
    #the next line creates an empty file so it won't run the next boot
    touch $FLAG
